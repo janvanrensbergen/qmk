2f8a15ec488f7f23d9d6790502eb687cf3ee9b8a
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2021-11-28 13:51:57 +0100

    default colemak

--- keyboards/crkbd/keymaps/janvanrensbergen/keymap.c
+++ keyboards/crkbd/keymaps/janvanrensbergen/keymap.c
@@ -23,7 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define U_NA KC_NO // present but not available for use
 #define U_NU KC_NO // available but not used
 
-enum layers { QWERTY, COLEMAK, NAV, NUM, SYM, FUN, MBO, MEDIA, MOUSE};
+enum layers { COLEMAK, QWERTY, NAV, NUM, SYM, FUN, MBO, MEDIA, MOUSE};
 
 #define U_RDO SCMD(KC_Z)
 #define U_PST LCMD(KC_V)
@@ -58,29 +58,29 @@ enum layers { QWERTY, COLEMAK, NAV, NUM, SYM, FUN, MBO, MEDIA, MOUSE};
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  [QWERTY] = LAYOUT_split_3x6_3(
-    //,--------------------------------------------------------.                         ,---------------------------------------------------------.
-    DF(COLEMAK),  KC_Q,     KC_W,    KC_E,    KC_R,      KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,      KC_P,   KC_BSPC,
-    //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
-        KC_LCTL, LCONTROL,LOPTION, LCOMMAND, LSHIFT,    KC_G,                               KC_H,     RSHIFT, RCOMMAND, ROPTION,  RCONTROL, KC_QUOT,
-    //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
-        KC_LSFT, KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,                              KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_ESC,
-    //|--------+---------+--------+--------+----------+--------+------------|  |---------+---------+---------+---------+---------+--------+--------|
-                                             MEDIA_ESC, NAV_TAB, MOUSE_SPC,      SYM_ENT, NUM_BSPC, FUN_DEL
-                                         //`--------------------------------'  `----------------------------'
-  ),
   [COLEMAK] = LAYOUT_split_3x6_3(
-    //,--------------------------------------------------------.                         ,---------------------------------------------------------.
-     DF(QWERTY),   KC_Q,    KC_W,     KC_F,    KC_P,     KC_G,                               KC_J,     KC_L,     KC_U,    KC_Y,    KC_QUOT, KC_BSPC,
-    //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
-        KC_LCTL, C_LCONTROL,C_LOPTION,C_LCOMMAND,C_LSHIFT,KC_D,                            KC_H,    C_RSHIFT,C_RCOMMAND,C_ROPTION,C_RCONTROL,KC_QUOT,
-    //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
-        KC_LSFT, KC_Z,    KC_X,     KC_C,     KC_V,      KC_B,                              KC_K,      KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, KC_ESC,
-    //|--------+---------+--------+--------+----------+--------+------------|  |---------+---------+---------+---------+---------+--------+--------|
-                                             MEDIA_ESC, NAV_TAB, MOUSE_SPC,      SYM_ENT, NUM_BSPC, FUN_DEL
-                                         //`--------------------------------'  `----------------------------'
-  ),
+      //,--------------------------------------------------------.                         ,---------------------------------------------------------.
+       DF(QWERTY),   KC_Q,    KC_W,     KC_F,    KC_P,     KC_G,                               KC_J,     KC_L,     KC_U,    KC_Y,    KC_QUOT, KC_BSPC,
+      //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
+          KC_LCTL, C_LCONTROL,C_LOPTION,C_LCOMMAND,C_LSHIFT,KC_D,                            KC_H,    C_RSHIFT,C_RCOMMAND,C_ROPTION,C_RCONTROL,KC_QUOT,
+      //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
+          KC_LSFT, KC_Z,    KC_X,     KC_C,     KC_V,      KC_B,                              KC_K,      KC_M,   KC_COMM,  KC_DOT,   KC_SLSH, KC_ESC,
+      //|--------+---------+--------+--------+----------+--------+------------|  |---------+---------+---------+---------+---------+--------+--------|
+                                               MEDIA_ESC, NAV_TAB, MOUSE_SPC,      SYM_ENT, NUM_BSPC, FUN_DEL
+                                           //`--------------------------------'  `----------------------------'
+    ),
+  [QWERTY] = LAYOUT_split_3x6_3(
+      //,--------------------------------------------------------.                         ,---------------------------------------------------------.
+      DF(COLEMAK),  KC_Q,     KC_W,    KC_E,    KC_R,      KC_T,                              KC_Y,     KC_U,     KC_I,    KC_O,      KC_P,   KC_BSPC,
+      //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
+          KC_LCTL, LCONTROL,LOPTION, LCOMMAND, LSHIFT,    KC_G,                               KC_H,     RSHIFT, RCOMMAND, ROPTION,  RCONTROL, KC_QUOT,
+      //|--------+---------+--------+--------+----------+--------|                         |---------+---------+---------+---------+--------+--------|
+          KC_LSFT, KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,                              KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, KC_ESC,
+      //|--------+---------+--------+--------+----------+--------+------------|  |---------+---------+---------+---------+---------+--------+--------|
+                                               MEDIA_ESC, NAV_TAB, MOUSE_SPC,      SYM_ENT, NUM_BSPC, FUN_DEL
+                                           //`--------------------------------'  `----------------------------'
+    ),
+
   [NAV] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
              U_NA,    RESET,   U_NA,    U_NA,    U_NA,    U_NA,                          U_NA, U_PST,   U_CPY,   U_CUT,   U_UND,  U_RDO,
@@ -160,6 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
@@ -167,6 +168,11 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   }
   return rotation;
 }
+
+#define L_BASE 0
+#define L_LOWER 2
+#define L_RAISE 4
+#define L_ADJUST 8
 
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
@@ -200,7 +206,6 @@ void oled_render_layer_state(void) {
             }
     }
 }
-
 
 char keylog_str[24] = {};
 
@@ -254,13 +259,14 @@ void oled_render_logo(void) {
     oled_write_P(crkbd_logo, false);
 }
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
     if (is_keyboard_master()) {
         oled_render_layer_state();
         oled_render_keylog();
     } else {
         oled_render_logo();
     }
+    return false;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -282,6 +288,14 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case RCOMMAND:
     case ROPTION:
     case RCONTROL:
+    case C_LCONTROL:
+    case C_LOPTION:
+    case C_LCOMMAND:
+    case C_LSHIFT:
+    case C_RSHIFT:
+    case C_RCOMMAND:
+    case C_ROPTION:
+    case C_RCONTROL:
         return TAPPING_TERM - 50;
     case MEDIA_ESC:
     case NAV_TAB:
@@ -307,6 +321,14 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
         case ROPTION:
         case RCONTROL:
         case MEDIA_ESC:
+        case C_LCONTROL:
+        case C_LOPTION:
+        case C_LCOMMAND:
+        case C_LSHIFT:
+        case C_RSHIFT:
+        case C_RCOMMAND:
+        case C_ROPTION:
+        case C_RCONTROL:
             return true;
         case NAV_TAB:
         case MOUSE_SPC:
